7fcd0e9b588cf37e4721ea57b7c1ace0d7f74cc3
Heiko Fanieng <heiko@fanieng.com>
2025-05-19 07:23:16 +0200

    feat: aktualisiere Import-Pfade für Komponenten und erweitere tsconfig mit Pfad-Alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mysteryland.biz',
 	integrations: [mdx(), sitemap()],
+	vite: {
+		resolve: {
+			alias: {
+				'@': path.resolve('./src'),
+				'@components': path.resolve('./src/components'),
+			},
+		},
+	},
 });
