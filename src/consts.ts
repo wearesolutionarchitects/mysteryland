e73e09133cbba52ed55451555fcb35432a57e657
Heiko Fanieng <heiko@fanieng.com>
2025-05-19 07:03:56 +0200

    feat: aktualisiere Meta-Tags für Discord, passe Footer-Links an und ändere Datumsformat auf Deutsch

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro events';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Mysteryland';
+export const SITE_DESCRIPTION = 'Willkommen auf meiner Website!';
