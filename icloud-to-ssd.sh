dc1ebcb88826269a052859ddf045da238b55a367
Heiko Fanieng <heiko@fanieng.com>
2025-06-20 21:56:10 +0200

    fix: entferne Album-Datum-Parameter aus dem Skript für iCloud-Downloads

--- icloud-to-ssd.sh
+++ icloud-to-ssd.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 # icloud-to-ssd.sh
 
-# 📅 Album-Datum als Parameter
-if [ -z "$1" ]; then
-  echo "❌ Bitte gib ein Datum im Format YYYY-MM-DD als Argument an, z. B.: 1979-09-29"
-  exit 1
-fi
-ALBUM_DATE="$1"
-
 # 📌 KONFIGURATION
 
 icloudpd \
   --username "heiko@fanieng.com" \
   --directory "/Volumes/Sandisk/Fotos" \
   --folder-structure "{:%Y/%m/%d}" \
-  --set-exif-datetime \
-  --xmp-sidecar \
-  --album "$ALBUM_DATE"
+  --recent "1" \
+  --set-exif-datetime
