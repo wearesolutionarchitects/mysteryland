1732b4ea2791740111c1dcfc6b7d664ac9b27727
Heiko Fanieng <heiko@fanieng.com>
2026-06-05 12:48:24 +0200

    chore: remove unused dependencies and update note syntax in documentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import starlight from '@astrojs/starlight';
 import path from 'path';
-import astroExpressiveCode from 'astro-expressive-code';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://mysteryland.biz',
     integrations: [
-        astroExpressiveCode(),
-        mdx(),
-        sitemap(),
         starlight({ 
             title: "Mysteryland",            
             
